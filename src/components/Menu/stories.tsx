72a23fd436882a92916ab7c7881be532668233b6
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-02-01 16:49:06 -0300

    feat: create container component

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -3,14 +3,13 @@ import Menu, { MenuProps } from '.';
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta;
 
 export const Default: Story<MenuProps> = (args) => <Menu {...args} />;
-
-Default.parameters = {
-  layout: 'fullscreen',
-  backgrounds: {
-    default: 'dark'
-  }
-};
