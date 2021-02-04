4e33ec69125464b4fa9a6afbe82d768e2fa6544d
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-02-04 17:26:31 -0300

    feat: create game details component

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,93 @@
+import { Apple, Linux, Windows } from '@styled-icons/fa-brands';
+
+import Heading from 'components/Heading';
+import MediaMatch from 'components/MediaMatch';
+
+import * as S from './styles';
+
+type Platform = 'windows' | 'linux' | 'mac';
+
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18';
+
+export type GameDetailsProps = {
+  developer: string;
+  genres: string[];
+  platforms: Platform[];
+  publisher: string;
+  rating: Rating;
+  releaseDate: string;
+};
+
+const GameDetails = ({
+  developer,
+  genres,
+  platforms,
+  publisher,
+  rating,
+  releaseDate
+}: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux title="Linux" size={18} />,
+    mac: <Apple title="Mac" size={18} />,
+    windows: <Windows title="Windows" size={18} />
+  };
+
+  const formatReleaseDate = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }).format(new Date(releaseDate));
+
+  const formatRating =
+    rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`;
+
+  const formatGenre = genres.join(' / ');
+
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
+
+      <S.Content>
+        <S.Block>
+          <S.Label>Developer</S.Label>
+          <S.Description>{developer}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Release Date</S.Label>
+          <S.Description>{formatReleaseDate}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Platforms</S.Label>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Publisher</S.Label>
+          <S.Description>{publisher}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>{formatRating}</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Genres</S.Label>
+          <S.Description>{formatGenre}</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  );
+};
+
+export default GameDetails;
