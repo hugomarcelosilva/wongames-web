62e7b79d5333c157b4607039f8e13667f1e146d7
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-02-09 16:58:47 -0300

    feat: create user dropdown component

---
+++ src/components/UserDropdown/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import UserDropdown, { UserDropdownProps } from '.';
+
+export default {
+  title: 'UserDropdown',
+  component: UserDropdown,
+  args: {
+    username: 'Hugo'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta;
+
+export const Default: Story<UserDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <UserDropdown {...args} />
+  </div>
+);
