ccb9fe00063f46dd23a3765f75312440320fb681
Hugo Silva <hugo.silva@arquivei.com.br>
2021-05-28 20:23:21 -0300

    feat: create e2e tests to sign up and sign in

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -7,9 +7,37 @@ describe('User', () => {
     const user = createUser();
     cy.visit('/sign-up');
 
-    cy.signUp(user)
+    cy.signUp(user);
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`);
     cy.findByText(user.username).should('exist');
+  });
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in');
+    cy.signIn();
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`);
+    cy.findByText('cypress').should('exist').click();
+    cy.findByText(/sign out/i).click();
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist');
+    cy.findByText('cypress').should('not.exist');
+  });
+
+  it.only('should sign in the user and redirect to the page that it was defined previously', () => {
+    cy.visit('/profile/me');
+
+    cy.location('href').should(
+      'eq',
+      `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/me`
+    );
+
+    cy.signIn();
+
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/me`);
+
+    cy.findByLabelText(/username/i).should('have.value', 'cypress');
+    cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com');
   });
 });
